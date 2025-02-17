48ff0a196c763bdfd7b2fd75b5efceb0dcaa3c14
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-02-17 10:34:16 -0500

    Update mod.rs

--- src/audit/stig/core/CAT1/mod.rs
+++ src/audit/stig/core/CAT1/mod.rs
@@ -1,7 +1,7 @@
-pub mod V253283;
+pub mod V253283_DEP;
 pub mod V253284_SEHOP;
 
 /// Reversible Password Decryption
-pub mod V253305;
-pub mod V253382;
-pub mod V253386;
+pub mod V253305_REVPASSWORD;
+pub mod V253382_RDA;
+pub mod V253386_AUTOPLAY;
